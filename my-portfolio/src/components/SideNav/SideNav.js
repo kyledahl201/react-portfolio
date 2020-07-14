80c24bc7f1a6e1fa4ea4209f9500a7df27778386
Kyle PC <kyle.dahl201@gmail.com>
2020-07-13 23:03:20 -0500

    creating sidenav component

---
+++ my-portfolio/src/components/SideNav/SideNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './sidenav.css'
+
+export const SideNav = (props) => {
+    return (
+        <div className="sidenav">
+            <img
+                src="./me.jpg"
+                className="avatar"
+                alt="avatar" />
+        </div>
+    )
+}
